288d513c216b3e4e582941c00d2f8a4c3c10f142
khakaa <phr0210@naver.com>
2023-06-21 01:43:39 +0900

    [ADD] : Home component 추가

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import PrivateRoute from "./routes/PrivateRoute";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Todo from "./pages/Todo";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route element={<PrivateRoute />}>
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
